6a3785b078a27f55b2f1f6cb2675b634bc83223d
Nauris Vasilevskis <navris@inbox.lv>
2023-10-01 18:59:26 +0300

    examples of pointers added

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+void function1(int *a){
+	printf("This function knows the value of var and can change it \n");
+	printf("Value before changing: %d: \n",*a);
+	*a = 111;
+	printf("Value set by function1 printed from function1: %d \n",*a);
+
+}
+//void function2()
 int main(){
-	printf("Hello world");
+	int *ptr = NULL;
+	int var = 10;
+	//ptr = &var;
+	printf("Variable from main function is : %d\n",var);
+	function1(&var);
+	printf("Variable after function1 but from main is : %d \n", var);
 	return 0;
+
 }
